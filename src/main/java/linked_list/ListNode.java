1ec62aa29485a72cbf09ecece7db45eda862a0d0
kongcheng123 <1175642794@qq.com>
2017-12-19 23:07:52 +0800

    添加 链表-去除连续重复的元素

---
+++ src/main/java/linked_list/ListNode.java
@@ -0,0 +1,8 @@
+package linked_list;
+
+/**
+ * @author xy
+ * @date 2017/12/19 22:04
+ */
+public class ListNode {
+}
---
+++ src/main/java/linked_list/RemoveDuplicatesfromSortedList.java
@@ -0,0 +1,8 @@
+package linked_list;
+
+/**
+ * @author xy
+ * @date 2017/12/19 22:05
+ */
+public class RemoveDuplicatesfromSortedList {
+}
